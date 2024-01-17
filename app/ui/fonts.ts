3c4cb7f997fb18a131b5b908c45cdb6f870ba417
to41207052 <to41207052@gmail.com>
2024-01-17 17:39:38 +0900

    test

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import {DotGothic16, Inter, Lusitana} from "next/font/google";
+
+export const inter  = Inter ({ subsets : ['latin']});
+
+export const lusitana = Lusitana ({
+    weight: ['400', '700'],
+    subsets: ['latin']});
+
+
+export const dotGothic16 = DotGothic16 ({
+    weight: ['400'],
+    subsets: ['latin']
+});
